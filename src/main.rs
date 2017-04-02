7171f44d45e3fb740f98deebe7372d7d8f98fada
yodang <yoda@elmekia.fr>
2017-04-02 23:40:37 +0200

    Basic bot implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+extern crate discord;
+use std::env;
+use discord::Discord;
+use discord::model::Event;
+
 fn main() {
-    println!("Hello");
+    let discord=Discord::from_bot_token(env::args().nth(1)).expect("login failed");
+
+    let (mut connection, _)=discord.connect().expect("connection failed");
+    println!("Connected!");
+
+    loop {
+        match connection.recv_event() {
+            Ok(Event::MessageCreate(message)) => 
+            {
+                println("{} says: {}", message.author.name, message.content);
+                if(message.content == "!say")
+                    discord.send_message(message.channel_id, "Hello o/", "", false);
+                else if(message.content == "!quit")
+                {
+                    println!("Quitting");
+                    break;
+                }
+
+            }
+            Ok(_) => {}
+            Err(discord::Error::closed(code, body)) =>
+            {
+                println!("Gateway sent error with code {:?}: {}", code, body);
+                break;
+            }
+            Err(err) =>
+            {
+                println!("Received error: {:?}", err);
+            }
+        }
+    }
 }
