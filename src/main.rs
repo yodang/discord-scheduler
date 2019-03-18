00b1db3e88e972318c6faa038ceea4f6ce589f78
yodang <yoda@elmekia.fr>
2019-03-18 14:30:17 +0100

    Switch to serenity

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,44 @@
-extern crate discord;
+#[macro_use] extern crate serenity;
 use std::env;
-use discord::Discord;
-use discord::model::Event;
+use std::collections::HashSet;
+
+use serenity::client::Client;
+use serenity::framework::StandardFramework;
+use serenity::prelude::*;
+use serenity::model::guild::{Role, Member};
+use serenity::model::channel::{GuildChannel, Message};
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    let discord=Discord::from_bot_token(args[1].as_str()).expect("login failed");
+    let mut discord=Client::new(args[1].as_str(), Handler).expect("login failed");
 
-    let (mut connection, _)=discord.connect().expect("connection failed");
-    println!("Connected!");
+    discord.with_framework(StandardFramework::new()
+        .configure(|c|
+            c.allow_dm(false)
+            .prefix("!")
+            //.allowed_channels()
+        )
+        .cmd("say", say)
+        .cmd("quit", quit)
+    );
 
-    loop {
-        match connection.recv_event() {
-            Ok(Event::MessageCreate(message)) => 
-            {
-                println!("{} says: {}", message.author.name, message.content);
-                if(message.content == "!say")
-                {
-                    discord.send_message(&message.channel_id, "Hello o/", "", false);
-                }
-                else if(message.content == "!quit")
-                {
-                    println!("Quitting");
-                    break;
-                }
-
-            }
-            Ok(_) => {}
-            Err(discord::Error::Closed(code, body)) =>
-            {
-                println!("Gateway sent error with code {:?}: {}", code, body);
-                break;
-            }
-            Err(err) =>
-            {
-                println!("Received error: {:?}", err);
-            }
-        }
+    if let Err(what)=discord.start()
+    {
+        println!("An error occured: {:?}", what);
     }
+
 }
+
+command!(
+    say(_context, message)
+    {
+        let _=message.channel_id.say("Hello o/");
+    }
+);
+
+command!(
+    quit(context, _message)
+    {
+        context.quit();
+    }
+);
